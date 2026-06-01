d86342e8c745978be69e9690c3318edd75ff696b
w1y0n0 <wiyono30@gmail.com>
2026-06-01 14:54:18 +0700

    Membuat Halaman Dashboard
    - Membuat Layout Admin
    - Membuat Controller Dashboard
    - Membuat Route Dashboard
    - Membuat View/Halaman Dashboard
    - Uji Coba Akses Halaman Dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,23 @@
 <?php
 
+use App\Http\Controllers\Admin\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
+});
+
+/**
+ * route for admin
+ */
+
+//group route with prefix "admin"
+Route::prefix('admin')->group(function () {
+
+    //group route with middleware "auth"
+    Route::group(['middleware' => 'auth'], function () {
+
+        //route dashboard
+        Route::get('/dashboard', [DashboardController::class, 'index'])->name('admin.dashboard.index');
+    });
 });
