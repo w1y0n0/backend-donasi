dff64f2b4113d957f506b2177e911cd742eb7997
w1y0n0 <wiyono30@gmail.com>
2026-06-03 08:24:22 +0700

    Membuat Restful API Donation
    - Membuat Controller API Donation
    - Membuat Route API Donation
    - Uji Coba API Donation

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Api\CampaignController;
 use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\DonationController;
 use App\Http\Controllers\Api\LoginController;
 use App\Http\Controllers\Api\ProfileController;
 use App\Http\Controllers\Api\RegisterController;
@@ -47,3 +48,10 @@ Route::get('/slider', [SliderController::class, 'index']);
 Route::get('/profile', [ProfileController::class, 'index'])->middleware('auth:api');
 Route::post('/profile', [ProfileController::class, 'update'])->middleware('auth:api');
 Route::post('/profile/password', [ProfileController::class, 'updatePassword'])->middleware('auth:api');
+
+/**
+ * Api Donation
+ */
+Route::get('/donation', [DonationController::class, 'index'])->middleware('auth:api');
+Route::post('/donation', [DonationController::class, 'store'])->middleware('auth:api');
+Route::post('/donation/notification', [DonationController::class, 'notificationHandler']);
